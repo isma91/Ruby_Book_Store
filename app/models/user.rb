479e730cff4657ab8368567ef529c435fa23a731
isma91 <ismaydogmus@gmail.com>
2016-09-19 15:04:34 +0200

    add function to find user by email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  def findByEmail(searchedEmail)
+    userToFind = false
+    User.all.each do |user|
+      if user.email == searchedEmail
+        userToFind = user
+        break
+      end
+    end
+    userToFind
+  end
 end
