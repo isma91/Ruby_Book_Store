8dfb1eddfe14310d39f3c2862515ff71a3dc660d
isma91 <ismaydogmus@gmail.com>
2016-09-19 16:02:40 +0200

    remove duplicate function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,2 @@
 class User < ApplicationRecord
-  def findByEmail(searchedEmail)
-    userToFind = false
-    User.all.each do |user|
-      if user.email == searchedEmail
-        userToFind = user
-        break
-      end
-    end
-    userToFind
-  end
 end
