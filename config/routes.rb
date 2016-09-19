f65c027795d7902eea76217f526019d50af8a03b
isma91 <ismaydogmus@gmail.com>
2016-09-19 12:15:55 +0200

    adding aliasses for login and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  #login page as root
+  root 'users#login'
+  #aliases for login page
+  get 'login' => 'users#login'
+  get 'user/login' => 'users#login'
+  #aliases for signup page
+  get 'signup' => 'users#new'
+  get 'user/create' => 'users#new'
+
+  post "login" => "users#login"
+  post 'user/create' => 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
