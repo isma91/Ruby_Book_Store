5c8b4e92c995819da89670163c779342a6d267c0
isma91 <ismaydogmus@gmail.com>
2016-09-20 21:49:42 +0200

    chang the type field to kind

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ApplicationRecord
+  validates :kind, presence: {
+      message: "Type can't be empty !!"
+  }
+  validates :idCustomer, presence: {
+      message: "Customer can't be empty !!"
+  }
+  validates :idBook, presence: {
+      message: "Book can't be empty !!"
+  }
 end
