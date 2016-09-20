75e4d85da236e03168a88010b58392dd36f0b329
isma91 <ismaydogmus@gmail.com>
2016-09-20 20:39:27 +0200

    require the session of user before every action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,12 @@
 class CustomersController < ApplicationController
+  before_action :requireLogin
+
+  def requireLogin
+    if session[:userId] == nil
+      flash[:fail] = "You must be logged !!"
+      redirect_to "/login"
+    end
+  end
 
   def new
     @customer = Customer.new
--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,17 @@
 class LogsController < ApplicationController
+  before_action :requireLogin
+
+  def requireLogin
+    if session[:userId] == nil
+      flash[:fail] = "You must be logged !!"
+      redirect_to "/login"
+    end
+  end
+
+  def index
+    user = User.find(session[:userId])
+    @userLastname = user.lastname
+    @userFirstname = user.firstname
+    @logs = Log.all
+  end
 end
