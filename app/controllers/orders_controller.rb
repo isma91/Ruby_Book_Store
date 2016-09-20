d50aecdcb9cd9eae36d5976277a94ad9bff5d47e
isma91 <ismaydogmus@gmail.com>
2016-09-20 21:51:14 +0200

    create order in the controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,4 +7,32 @@ class OrdersController < ApplicationController
       redirect_to "/login"
     end
   end
+
+  def new
+    @order = Order.new
+    @books = Book.all
+    @customers = Customer.all
+  end
+
+  def create
+    @books = Book.all
+    @customers = Customer.all
+    @order = Order.new
+    @order.kind = params[:kind]
+    @order.idBook = params[:book]
+    @order.idCustomer = params[:customer]
+    if @order.save
+      time = Time.new
+      currentTime = time.strftime("%d-%m-%Y %H:%M:%S")
+      Log.new(date: currentTime, userId: session[:userId], action: "add order", bookCustomerId: @order.id).save
+      flash[:success] = "Order added successfully !!"
+      redirect_to "/orders"
+    else
+      render "new"
+    end
+  end
+
+  def index
+    @orders = Order.all
+  end
 end
