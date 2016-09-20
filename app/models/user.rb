8c73be93a497b0403f17dfc8194d826dc9e75510
isma91 <ismaydogmus@gmail.com>
2016-09-20 10:00:29 +0200

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  validates :lastname, presence: {
+      message: "Lastname can't be empty !!"
+  }
+  validates :firstname, presence: {
+      message: "Firstname can't be empty !!"
+  }
+  validates :email, uniqueness: {
+      message: "Email already taken !!"
+  }
+  validates :pass, presence: {
+      message: "Pass can't be empty !!"
+  }
 end
