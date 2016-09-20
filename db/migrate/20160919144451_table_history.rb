cb275e3f2241516386716e933f7d619070b9d449
isma91 <ismaydogmus@gmail.com>
2016-09-20 09:59:27 +0200

    adding book, customer, logs, user and orders test

--- db/migrate/20160919144451_table_history.rb
+++
@@ -1,9 +0,0 @@
-class TableHistory < ActiveRecord::Migration[5.0]
-  def change
-    create_table :histories do |t|
-      t.datetime :date
-      t.string :user
-      t.string :action
-    end
-  end
-end
