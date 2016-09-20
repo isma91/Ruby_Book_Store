d81780ebbd9c3c55ad764a6861dbeb0b0ff1af0a
isma91 <ismaydogmus@gmail.com>
2016-09-20 20:39:38 +0200

    require the session of user before every action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :requireLogin
+
+  def requireLogin
+    if session[:userId] == nil
+      flash[:fail] = "You must be logged !!"
+      redirect_to "/login"
+    end
+  end
 end
