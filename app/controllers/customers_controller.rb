cfafc5eb45f9e99b574842ddfbaad9a2e3de88db
isma91 <ismaydogmus@gmail.com>
2016-09-20 17:31:18 +0200

    add the create and list customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,32 @@
 class CustomersController < ApplicationController
+
+  def new
+    @customer = Customer.new
+  end
+
+  def index
+    user = User.find(session[:userId])
+    @userLastname = user.lastname
+    @userFirstname = user.firstname
+    @customers = Customer.all
+  end
+
+  def create
+    @customer = Customer.new(customerParam)
+    if @customer.save
+      time = Time.new
+      currentTime = time.strftime("%d-%m-%Y %H:%M:%S")
+      Log.new(date: currentTime, userId: session[:userId], action: "add customer", bookCustomerId: @customer.id).save
+      flash[:success] = "Customer added successfully !!"
+      redirect_to "/customers"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def customerParam
+    params.require(:customer).permit(:lastname, :firstname, :adresse, :city, :email)
+  end
 end
