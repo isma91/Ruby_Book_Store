c2ccbf04c5b2c6be9b108a7343d7542af7191e2b
isma91 <ismaydogmus@gmail.com>
2016-09-20 13:12:54 +0200

    validate book form

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,20 @@
 class Book < ApplicationRecord
+  validates :name, presence: {
+      message: "Name can't be empty !!"
+  }
+  validates :author, presence: {
+      message: "Author can't be empty !!"
+  }
+  validates :year, presence: {
+      message: "Year can't be empty !!"
+  }
+  validates :kind, presence: {
+      message: "Kind can't be empty !!"
+  }
+  validates :cover, presence: {
+      message: "Cover can't be empty !!"
+  }
+  validates :Resume, presence: {
+      message: "Resume can't be empty !!"
+  }
 end
