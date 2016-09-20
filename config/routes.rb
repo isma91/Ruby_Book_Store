3b0b994ca2b1a74e5f820b2b3b23177a0382470d
isma91 <ismaydogmus@gmail.com>
2016-09-20 13:10:24 +0200

    adding the create book in routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   get 'login' => 'users#login'
   get 'signup' => 'users#new'
   get 'books' => 'books#index'
+  get 'book/new' => 'books#new'
 
   post "user/login" => "users#connexion"
   post 'user/create' => 'users#create'
   post 'logout' => 'sessions#logout'
+  post 'book/create' => 'books#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
