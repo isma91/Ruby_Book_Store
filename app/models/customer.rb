db7de5f079084bddfa1fb2d622fb7f9b6f8be86a
isma91 <ismaydogmus@gmail.com>
2016-09-20 17:30:59 +0200

    add validate field

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,17 @@
 class Customer < ApplicationRecord
+  validates :lastname, presence: {
+      message: "Lastname can't be empty !!"
+  }
+  validates :firstname, presence: {
+      message: "Firstname can't be empty !!"
+  }
+  validates :email, uniqueness: {
+      message: "Email already taken !!"
+  }
+  validates :adresse, presence: {
+      message: "Adresse can't be empty !!"
+  }
+  validates :city, presence: {
+      message: "City can't be empty !!"
+  }
 end
