d523ef5f6779228006828ee799a62317546cd9c8
isma91 <ismaydogmus@gmail.com>
2016-09-20 13:12:37 +0200

    bcrypt the pass before save and remove the pass after the password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  before_save :bcryptPass
+  after_save :clearPass
+
   validates :lastname, presence: {
       message: "Lastname can't be empty !!"
   }
@@ -11,4 +14,14 @@ class User < ApplicationRecord
   validates :pass, presence: {
       message: "Pass can't be empty !!"
   }
+  validates_length_of :pass, :in => 5..20, :on => :create
+
+  def bcryptPass
+    if pass.present?
+      self.pass= BCrypt::Password.create(pass)
+    end
+  end
+  def clearPass
+    self.pass = nil
+  end
 end
