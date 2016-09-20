d1110710bf3dd95babf1e2374c45e1b5471e4289
isma91 <ismaydogmus@gmail.com>
2016-09-20 20:38:14 +0200

    add logs display in routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   root 'users#login'
 
-  get 'login' => 'users#login'
-  get 'user/login' => 'users#login'
-  get 'signup' => 'users#new'
-  get 'user/create' => 'users#new'
-  get 'books' => 'books#index'
-  get 'book/new' => 'books#new'
-  get 'customer/new' => 'customers#new'
-  get 'customers' => 'customers#index'
-  get 'customer/create' => 'customers#new'
+  get 'login'            => 'users#login'
+  get 'user/login'       => 'users#login'
+  get 'signup'           => 'users#new'
+  get 'user/create'      => 'users#new'
+  get 'books'            => 'books#index'
+  get 'book/new'         => 'books#new'
+  get 'customer/new'     => 'customers#new'
+  get 'customers'        => 'customers#index'
+  get 'customer/create'  => 'customers#new'
+  get 'logs'             => 'logs#index'
 
-  post "user/login" => "users#connexion"
-  post 'user/create' => 'users#create'
-  post 'logout' => 'sessions#logout'
-  post 'book/create' => 'books#create'
+  post "user/login"      => "users#connexion"
+  post 'user/create'     => 'users#create'
+  post 'logout'          => 'sessions#logout'
+  post 'book/create'     => 'books#create'
   post 'customer/create' => 'customers#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
