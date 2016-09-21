9ef6df38c52caffd680ed4fa0da7dd2f99a75fdf
isma91 <ismaydogmus@gmail.com>
2016-09-21 20:25:32 +0200

    display 404 error to every error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :failed, :info
+  rescue_from ActionController::RoutingError, :with => :pageNotFound
+  rescue_from StandardError, :with => :pageNotFound
+  rescue_from ActionController::UnknownController, :with => :pageNotFound
+  rescue_from ActiveRecord::RecordNotFound, :with => :pageNotFound
+
+  def pageNotFound
+    render "errors/404.html", :status => 404
+  end
 end
