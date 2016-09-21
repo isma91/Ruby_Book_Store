961ece805b3d9c26a988fd8647f76c264311863b
isma91 <ismaydogmus@gmail.com>
2016-09-21 22:14:48 +0200

    remove unusued rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :failed, :info
   rescue_from ActionController::RoutingError, :with => :pageNotFound
   rescue_from StandardError, :with => :pageNotFound
-  rescue_from ActionController::UnknownController, :with => :pageNotFound
-  rescue_from ActiveRecord::RecordNotFound, :with => :pageNotFound
 
   def pageNotFound
     render "errors/404.html", :status => 404
