c558c7ebad060a176ec9c56867c55a6d95381e3a
isma91 <ismaydogmus@gmail.com>
2016-09-21 22:15:43 +0200

    remove type field

--- app/assets/javascripts/books/new.js
+++ app/assets/javascripts/books/new.js
@@ -30,15 +30,4 @@ $(document).on('turbolinks:load', function () {
             $('#resumeCount').css('color', '#000000');
         }
     });
-    /* kind of book can be a novel or a manga, if it's a manga display new kind */
-    $(document).on('change', 'select[name=type]', function(event) {
-        event.preventDefault();
-        if ($(this).val() === "manga") {
-            $('#selectKind').html('<option value="kodomo">Kodomo (For Young Child)</option><option value="shojo">Shojo (For Teenage Girl)</option><option value="shonen">Shonen (For Teen Boy)</option><option value="josei">Josei (For Young Women and Adult)</option><option value="seinen">Seinen (For Young Man and Adult)</option><option value="redisu">Redisu (For Adult Woman)</option><option value="seijin">Seijin (For Adult Man)</option>');
-        } else {
-            $('#selectKind').html('<option value="action">Action</option><option value="adventure">Adventure</option><option value="detective">Detective</option><option value="drama">Drama</option><option value="erotic">Erotic</option><option value="fantasy">Fantasy</option><option value="horror">Horror</option><option value="humour">Humour</option><option value="legend">Legend</option><option value="mystery">Mystery</option><option value="mythology">Mythology</option><option value="romance">Romance</option><option value="science fiction">Science Fiction</option><option value="western">Western</option>');
-        }
-        $('select').material_select();
-        $('#labelKind').html('Kind Of ' + $(this).val().charAt(0).toUpperCase() + $(this).val().slice(1));
-    });
 });
