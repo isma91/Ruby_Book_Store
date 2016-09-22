7afb16403d9b8783420162d9b6b177388bc0c5d8
isma91 <ismaydogmus@gmail.com>
2016-09-22 16:13:34 +0200

    require the loged user + add all users display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
+  before_action :requireLogin
+
+  def requireLogin
+    if session[:userId] == nil
+      flash[:fail] = "You must be logged !!"
+      redirect_to "/login"
+    end
+  end
 
   def logout
     time = Time.new
@@ -7,5 +15,12 @@ class SessionsController < ApplicationController
     reset_session
     flash[:success] = "You logout successfully !!"
     redirect_to "/login"
+  end
+
+  def index
+    user = User.find(session[:userId])
+    @userLastname = user.lastname
+    @userFirstname = user.firstname
+    @users = User.all
   end
 end
