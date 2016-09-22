e483326ffdd53d2f1f2d154bed4e0345a546cb58
isma91 <ismaydogmus@gmail.com>
2016-09-22 10:00:28 +0200

    display error 500 if something is wrong

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :failed, :info
   rescue_from ActionController::RoutingError, :with => :pageNotFound
-  rescue_from StandardError, :with => :pageNotFound
+  rescue_from StandardError, :with => :pageError
 
   def pageNotFound
     render "errors/404.html", :status => 404
+  end
+
+  def pageError
+    render "errors/500.html", :status => 500
   end
 end
