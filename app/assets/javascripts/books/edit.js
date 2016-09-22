ef0f913b0ccec9731400fa7d0726480f038c2af1
isma91 <ismaydogmus@gmail.com>
2016-09-22 09:59:19 +0200

    display the file input if we want to change the cover

--- app/assets/javascripts/books/edit.js
+++ app/assets/javascripts/books/edit.js
@@ -1,3 +1,21 @@
-/**
- * Created by isma91 on 22/09/16.
- */
+/*jslint browser: true, node : true*/
+/*jslint devel : true*/
+/*global $, document, this*/
+//$(document).ready(function() {
+$(document).on('turbolinks:load', function () {
+    /* initialize the materialize select */
+    $('select').material_select();
+    /* initialize the materialize datepicker with personnal limit date */
+    $('.datepicker').pickadate({
+        selectMonths: true,
+        selectYears: 200
+    });
+    $(document).on('change', '#editCoverSwitch', function(event) {
+        event.preventDefault();
+        if ($(this).is(':checked') === true) {
+            $('#editCover').html('<div class="row"><div class="file-field input-field"><div class="btn"><span>File</span><input type="file" accept="image/*" name="cover"></div><div class="file-path-wrapper"><input class="file-path" type="text" id="filePath"></div></div>');
+        } else {
+            $('#editCover').html('');
+        }
+    });
+});
