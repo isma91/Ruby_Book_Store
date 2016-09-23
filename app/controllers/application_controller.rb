ec2dfc59b62486863742c2f4a044d8a75bf0bd25
Ismail Aydogmus <ismaydogmus@gmail.com>
2016-09-23 12:30:23 +0200

    remove the comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :failed, :info
   rescue_from ActionController::RoutingError, :with => :pageNotFound
-  #rescue_from StandardError, :with => :pageError
+  rescue_from StandardError, :with => :pageError
 
   def pageNotFound
     render "errors/404.html", :status => 404
